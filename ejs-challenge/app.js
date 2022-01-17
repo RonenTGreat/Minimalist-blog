3127fcdf623bb65a8e56263d071c30703669fdb7
ronentgreat <ronenh53@gmail.com>
2022-01-17 20:58:12 +0000

    rendered the post array to the home page

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { startingParagraph: homeStartingContent });
+  res.render("home", { startingParagraph: homeStartingContent, paragraph: posts});
   console.log(posts);
 });
 
