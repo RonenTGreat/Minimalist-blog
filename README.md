7ca26a2256958a23a9e32afadf180a9b26e1e480
ronentgreat <ronenh53@gmail.com>
2022-02-02 11:39:07 +0000

    .

--- README.md
+++ README.md
@@ -4,27 +4,31 @@
 
 A minimalist blog site using EJS, Node/Express.
 
-## Installation 
+## Installation
 
 Make sure you have `node` installed on your computer
 
 1. Clone this repo
+   
 ```bash
 $ git clone https://github.com/RonenTGreat/Minimalist-blog.git
 ```
 
 2. Navigate to `directory`
+
 ```bash
 $ cd minimalist-blog
 ```
 
 
 3. Install `dependencies`
+
 ```bash
 $ npm i
 ```
 
-1. Run app
+4. Run app
+
 ```bash
 $ node app.js
 ```
--- app.js
+++ app.js
@@ -4,69 +4,83 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose")
 
 const homeStartingContent =
   "Don’t bend, don’t water it down, don’t try to make it logical, don’t edit your own soul according to the fashion. Rather, follow your most intense obsessions mercilessly. Keeping a journal of what’s going on in your life is a good way to help you distill what’s important and what’s not";
 
+const aboutContent =
+  "Helping you to reach your full potential by making sure you reach your goals through daily jorunalling.";
 
-const aboutContent = "Helping you to reach your full potential by making sure you reach your goals through daily jorunalling.";
 const contactContent =
   "Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within a matter of hours to help you.";
 
-let posts = [];
-
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", { startingParagraph: homeStartingContent, paragraph: posts});
+mongoose.connect("mongodb://localhost:27017/journalDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {aboutParagraph: aboutContent});
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/compose", (req, res) => {
-  res.render("compose")
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
-
-app.get("/contact", (req, res) => {
-  res.render("contact", { contactParagraph: contactContent });
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
-app.get("/posts/:topic", (req, res) => {
-  for(let i = 0; i < posts.length; i++){
-    if (_.lowerCase(req.params.topic) === _.lowerCase(posts[i].title)) {
-      res.render("post", {head: posts[i].title, body: posts[i].body})
-    }
-  }
-})
+app.post("/compose", function(req, res){
 
-
-app.post("/compose", (req, res) => {
-
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
-    body : req.body.postArea
-  };
+    content: req.body.postBody
+  });
 
-  posts.push(post);
-  res.redirect('/')
+  post.save(function(err){
+    if(!err){
+  res.redirect("/");
+    }
+  });
+
 
 });
 
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
 
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
 
+});
 
-
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
